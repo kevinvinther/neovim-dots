ed9fa30bc5e6b9dc464cc7d0eeb2bf5fbda9d235
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2021-09-16 12:26:22 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#! /bin/usr/sh
+
+if [ `whoami` != root ]; then
+    echo Please run this script as root or using sudo
+    exit
+fi
+
+echo "Do you have neovim nightly installed?"
+
+read x
+
+if [ -d ~/.cconfig/nvim/ ]; then
+    echo "Found neovim config folder"
+elif [ "$x" == "n" ]; then 
+    echo "If you haven't installed neovim nightly, you can do so by doing:"
+    echo "  macOS:  brew install --HEAD luajit"
+    echo "          brew install --HEAD neovim"
+    echo "  arch:   yay -S neovim-git (assuming yay installed)"
+    echo "  other:  download from https://github.com/neovim/neovim/releases/tag/nightly"
+    exit
+else
+    echo "Did not find neovim config folder, did you change the location from ~/.config/nvim?"
+    exit
+fi
+
+echo "Copying files to neovim directory"
+cp -r ./ ~/.config/nvim/
+echo "Finished copying files"
+
+echo "Opening Neovim. Follow instructions accordingly"
+nvim +PackerInstall
